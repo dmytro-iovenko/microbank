cac06e6761a57b54dac0723d3c084ccfe11d0724
Dmytro Iovenko <iovenko@gmail.com>
2023-07-21 18:38:08 -0400

    creating command handler interface

---
+++ account-service/account-command/src/main/java/com/microbank/account/command/handlers/CommandHandler.java
@@ -0,0 +1,13 @@
+package com.microbank.account.command.handlers;
+
+import com.microbank.account.command.commands.CloseAccountCommand;
+import com.microbank.account.command.commands.DepositFundsCommand;
+import com.microbank.account.command.commands.OpenAccountCommand;
+import com.microbank.account.command.commands.WithdrawFundsCommand;
+
+public interface CommandHandler {
+    void handle(OpenAccountCommand command);
+    void handle(DepositFundsCommand command);
+    void handle(WithdrawFundsCommand command);
+    void handle(CloseAccountCommand command);
+}
