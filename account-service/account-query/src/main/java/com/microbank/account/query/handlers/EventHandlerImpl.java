e13d20df5d169f6a377d0d12bbc4fbb1091c347d
Dmytro Iovenko <iovenko@gmail.com>
2023-07-27 18:24:01 -0400

    implementing AccountOpenedEvent handler

--- account-service/account-query/src/main/java/com/microbank/account/query/handlers/EventHandlerImpl.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/handlers/EventHandlerImpl.java
@@ -7,6 +7,7 @@ import com.microbank.account.core.events.AccountClosedEvent;
 import com.microbank.account.core.events.AccountOpenedEvent;
 import com.microbank.account.core.events.FundsDepositedEvent;
 import com.microbank.account.core.events.FundsWithdrawnEvent;
+import com.microbank.account.query.entities.Account;
 import com.microbank.account.query.repositories.AccountRepository;
 
 @Service
@@ -16,26 +17,33 @@ public class EventHandlerImpl implements EventHandler {
 
     @Override
     public void on(AccountOpenedEvent event) {
-        // TODO Auto-generated method stub
-        
+        Account account = Account.builder()
+                .id(event.getId())
+                .accountHolder(event.getAccountHolder())
+                .creationDate(event.getCreateDate())
+                .accountType(event.getAccountType())
+                .balance(event.getOpeningBalance())
+                .build();
+
+        accountRepository.save(account);
     }
 
     @Override
     public void on(FundsDepositedEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void on(FundsWithdrawnEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void on(AccountClosedEvent event) {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
 }
