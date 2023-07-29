a59caf4f3343a115e16acd39d48069a8bb58ea89
Dmytro Iovenko <iovenko@gmail.com>
2023-07-28 21:10:01 -0400

    updating annotations to fix error

--- account-service/account-command/src/main/java/com/microbank/account/command/commands/BaseCommand.java
+++ account-service/account-command/src/main/java/com/microbank/account/command/commands/BaseCommand.java
@@ -1,9 +1,13 @@
 package com.microbank.account.command.commands;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @SuperBuilder
 public abstract class BaseCommand {
     private String id;
