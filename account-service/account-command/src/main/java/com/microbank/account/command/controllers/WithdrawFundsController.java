f73d202c7de562d2cf0b1550e89de437a34e576e
Dmytro Iovenko <iovenko@gmail.com>
2023-07-29 13:47:52 -0400

    creating withdraw funds controller

---
+++ account-service/account-command/src/main/java/com/microbank/account/command/controllers/WithdrawFundsController.java
@@ -0,0 +1,35 @@
+package com.microbank.account.command.controllers;
+
+import java.text.MessageFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microbank.account.command.commands.WithdrawFundsCommand;
+import com.microbank.account.command.services.CommandDispatcher;
+import com.microbank.account.core.dto.BaseResponse;
+
+@RestController
+@RequestMapping(path = "/api/v1/withdrawFunds")
+public class WithdrawFundsController {
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable(value = "id") String id, @RequestBody WithdrawFundsCommand command) {
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("Withdraw funds request completed succesfully!"), HttpStatus.OK);
+        } catch (Exception e) {
+            String safeErrorMessage = MessageFormat.format("Error while processing request to withdraw funds from bank account with id - {0}", id);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }    
+}
