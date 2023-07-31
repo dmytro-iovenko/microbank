28215ced7eb72ed748d0141a5eb72ec1ec1f8a9c
Dmytro Iovenko <iovenko@gmail.com>
2023-07-31 19:52:47 -0400

    configuring kafka consumer

--- account-service/account-query/src/main/java/com/microbank/account/query/kafka/EventConsumerImpl.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/kafka/EventConsumerImpl.java
@@ -16,24 +16,28 @@ public class EventConsumerImpl implements EventConsumer {
     @Autowired
     private EventHandler eventHandler;
     
+    @KafkaListener(topics = "AccountOpenedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(AccountOpenedEvent event, Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
 
+    @KafkaListener(topics = "FundsDepositedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(FundsDepositedEvent event, Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
 
+    @KafkaListener(topics = "FundsWithdrawnEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(FundsWithdrawnEvent event, Acknowledgment ack) {
         eventHandler.on(event);
         ack.acknowledge();
     }
 
+    @KafkaListener(topics = "AccountClosedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(AccountClosedEvent event, Acknowledgment ack) {
         eventHandler.on(event);
