86638a0d898314ed57579d313f9e2d68fe2151ec
Dmytro Iovenko <iovenko@gmail.com>
2023-07-31 19:46:10 -0400

    implementing event consumer

---
+++ account-service/account-query/src/main/java/com/microbank/account/query/kafka/EventConsumerImpl.java
@@ -0,0 +1,42 @@
+package com.microbank.account.query.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Service;
+
+import com.microbank.account.core.events.AccountClosedEvent;
+import com.microbank.account.core.events.AccountOpenedEvent;
+import com.microbank.account.core.events.FundsDepositedEvent;
+import com.microbank.account.core.events.FundsWithdrawnEvent;
+import com.microbank.account.query.handlers.EventHandler;
+
+@Service
+public class EventConsumerImpl implements EventConsumer {
+    @Autowired
+    private EventHandler eventHandler;
+    
+    @Override
+    public void consume(AccountOpenedEvent event, Acknowledgment ack) {
+        eventHandler.on(event);
+        ack.acknowledge();
+    }
+
+    @Override
+    public void consume(FundsDepositedEvent event, Acknowledgment ack) {
+        eventHandler.on(event);
+        ack.acknowledge();
+    }
+
+    @Override
+    public void consume(FundsWithdrawnEvent event, Acknowledgment ack) {
+        eventHandler.on(event);
+        ack.acknowledge();
+    }
+
+    @Override
+    public void consume(AccountClosedEvent event, Acknowledgment ack) {
+        eventHandler.on(event);
+        ack.acknowledge();
+    }
+}
