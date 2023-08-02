7cbe0c486e04cf7ee5b3a53cbb5a091bd5078751
Dmytro Iovenko <iovenko@gmail.com>
2023-08-01 20:14:55 -0400

    implementing query dispatcher

--- account-service/account-query/src/main/java/com/microbank/account/query/dispatchers/QueryDispatcher.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/dispatchers/QueryDispatcher.java
@@ -8,7 +8,7 @@ import com.microbank.account.query.queries.BaseQuery;
 public interface QueryDispatcher {
     @FunctionalInterface
     public interface QueryHandlerMethod<T extends BaseQuery> {
-        void handle(T command);
+        List<Account> handle(T query);
     }
     <T extends BaseQuery> void registerHandler(Class<T> type, QueryHandlerMethod<T> handler);
     List<Account> send(BaseQuery query);
---
+++ account-service/account-query/src/main/java/com/microbank/account/query/dispatchers/QueryDispatcherImpl.java
@@ -0,0 +1,32 @@
+package com.microbank.account.query.dispatchers;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.microbank.account.query.entities.Account;
+import com.microbank.account.query.queries.BaseQuery;
+
+public class QueryDispatcherImpl implements QueryDispatcher {
+        private final Map<Class<? extends BaseQuery>, List<QueryHandlerMethod>> routes = new HashMap<>();
+
+    @Override
+    public <T extends BaseQuery> void registerHandler(Class<T> type, QueryHandlerMethod<T> handler) {
+        var handlers = routes.computeIfAbsent(type, c -> new LinkedList<>());
+        handlers.add(handler);
+    }
+
+    @Override
+    public List<Account> send(BaseQuery query) {
+        var handlers = routes.get(query.getClass());
+        if (handlers == null || handlers.size() == 0) {
+            throw new RuntimeException("No query handler was registered!");
+        }
+        if (handlers.size() > 1) {
+            throw new RuntimeException("Cannot send query to more than one handler!");
+        }
+        return handlers.get(0).handle(query);
+    }
+
+}
