656cdb51f0d2a491e9c75af025cd7b5c3571be0a
Dmytro Iovenko <iovenko@gmail.com>
2023-08-02 21:45:39 -0400

    implementing FindAllAccountsQuery handler

--- account-service/account-query/src/main/java/com/microbank/account/query/handlers/QueryHandlerImpl.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/handlers/QueryHandlerImpl.java
@@ -1,18 +1,27 @@
 package com.microbank.account.query.handlers;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.microbank.account.query.entities.Account;
 import com.microbank.account.query.queries.FindAccountByIdHolderQuery;
 import com.microbank.account.query.queries.FindAccountByIdQuery;
 import com.microbank.account.query.queries.FindAccountsWithBalanceQuery;
 import com.microbank.account.query.queries.FindAllAccountsQuery;
+import com.microbank.account.query.repositories.AccountRepository;
 
 public class QueryHandlerImpl implements QueryHandler {
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Override
     public List<Account> handle(FindAllAccountsQuery query) {
-        return null;
+        Iterable<Account> accounts = accountRepository.findAll();
+        List<Account> accountsList = new ArrayList<>();
+        accounts.forEach(accountsList::add);
+        return accountsList;
     }
 
     @Override
