c8e54d932a1b9e27bb13f01d995f9c36b01d1e17
Dmytro Iovenko <iovenko@gmail.com>
2023-08-02 22:01:17 -0400

    implementing FindAccountByIdQuery handler

--- account-service/account-query/src/main/java/com/microbank/account/query/handlers/QueryHandlerImpl.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/handlers/QueryHandlerImpl.java
@@ -2,6 +2,7 @@ package com.microbank.account.query.handlers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -26,7 +27,11 @@ public class QueryHandlerImpl implements QueryHandler {
 
     @Override
     public List<Account> handle(FindAccountByIdQuery query) {
-        return null;
+        Optional<Account> account = accountRepository.findById(query.getId());
+        if (account.isEmpty()) return null;
+        List<Account> accountsList = new ArrayList<>();
+        accountsList.add(account.get());
+        return accountsList;
     }
 
     @Override
