a40d884181c8e22dca160821b382b33182f4a8c7
Dmytro Iovenko <iovenko@gmail.com>
2023-08-03 20:48:16 -0400

    adding getAccountById

--- account-service/account-query/src/main/java/com/microbank/account/query/controllers/AccountLookupController.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/controllers/AccountLookupController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microbank.account.query.dispatchers.QueryDispatcher;
 import com.microbank.account.query.dto.AccountLookupResponse;
 import com.microbank.account.query.entities.Account;
+import com.microbank.account.query.queries.FindAccountByIdQuery;
 import com.microbank.account.query.queries.FindAllAccountsQuery;
 
 @RestController
@@ -39,4 +41,21 @@ public class AccountLookupController {
         }
     }
 
+    @GetMapping(path = "/byId/{id}")
+    public ResponseEntity<AccountLookupResponse> getAccountById(@PathVariable(value = "id") String id) {
+        try {
+            List<Account> accounts = queryDispatcher.send(new FindAccountByIdQuery(id));
+            if (accounts == null || accounts.size() == 0) {
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
+            AccountLookupResponse response = AccountLookupResponse.builder()
+                    .accounts(accounts)
+                    .message("Succesfully returned bank account")
+                    .build();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            String safeErrorMessage = "Failed to complete get account by ID request!";
+            return new ResponseEntity<>(new AccountLookupResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- account-service/account-query/src/main/java/com/microbank/account/query/queries/FindAccountByIdQuery.java
+++ account-service/account-query/src/main/java/com/microbank/account/query/queries/FindAccountByIdQuery.java
@@ -1,8 +1,12 @@
 package com.microbank.account.query.queries;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FindAccountByIdQuery implements BaseQuery {
     private String id;
 }
