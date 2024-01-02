c51e8422617a248e59f51b602a4c75d4318e9b11
Dmytro Iovenko <iovenko@gmail.com>
2024-01-01 19:52:55 -0500

    registering account command microservice with Eureka

--- account-service/account-command/src/main/java/com/microbank/account/command/AccountCommandApplication.java
+++ account-service/account-command/src/main/java/com/microbank/account/command/AccountCommandApplication.java
@@ -3,6 +3,7 @@ package com.microbank.account.command;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.microbank.account.command.commands.CloseAccountCommand;
 import com.microbank.account.command.commands.DepositFundsCommand;
@@ -13,6 +14,7 @@ import com.microbank.account.command.services.CommandHandler;
 
 import jakarta.annotation.PostConstruct;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountCommandApplication {
 	@Autowired
