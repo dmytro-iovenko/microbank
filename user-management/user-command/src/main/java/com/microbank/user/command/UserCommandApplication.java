3d71e34916427bff7b456a7be0f06e243f629850
Dmytro Iovenko <iovenko@gmail.com>
2024-01-13 21:40:39 -0500

    registering user command handlers

--- user-management/user-command/src/main/java/com/microbank/user/command/UserCommandApplication.java
+++ user-management/user-command/src/main/java/com/microbank/user/command/UserCommandApplication.java
@@ -1,13 +1,33 @@
 package com.microbank.user.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microbank.base.core.dispatchers.CommandDispatcher;
+import com.microbank.user.command.commands.DeleteUserCommand;
+import com.microbank.user.command.commands.RegisterUserCommand;
+import com.microbank.user.command.commands.UpdateUserCommand;
+import com.microbank.user.command.services.UserCommandHandler;
+
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class UserCommandApplication {
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+	@Autowired
+	private UserCommandHandler commandHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserCommandApplication.class, args);
+	}
+
+	@PostConstruct
+	public void registerHandler() {
+		commandDispatcher.registerHandler(RegisterUserCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(UpdateUserCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DeleteUserCommand.class, commandHandler::handle);
 	}
 
 }
